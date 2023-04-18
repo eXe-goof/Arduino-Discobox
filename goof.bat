14d467dcd93b8ee8dd6fab9ef18e0c90e5e596ba
eXe <110734447+eXe-goof@users.noreply.github.com>
2023-04-18 21:11:36 +0200

    Update goof.bat

--- goof.bat
+++ goof.bat
@@ -1,12 +1,10 @@
 @rem *This file is use to compile c/c++ programs with MingGW*
 @rem *You have to add this to path to execute it everywhere*
 @rem *This needs a single argument*
+@rem *This is not Excption secured*
 
 @echo off
-if (%1 == EXISTS) (
   g++ %1
   a.exe
-) else (
-  color 4
-  echo Can't find file: %1
-)
+
+@rem *Could produce Error if .exe File is not called "a.exe"*
