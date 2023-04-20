05361830bb6e08ede7037c2e886d31226816cfa6
eXe <110734447+eXe-goof@users.noreply.github.com>
2023-04-20 19:01:36 +0200

    Create goof.bat

---
+++ goof.bat
@@ -0,0 +1,8 @@
+@rem *This file is use to compile c/c++ programs with MingGW*
+@rem *You have to add this to path to execute it everywhere*
+
+@echo off
+  g++ %1
+  a.exe
+
+@rem *Could produce Error if .exe File is not called "a.exe"*
